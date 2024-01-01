662fe5590a3b9af1b49e3b1ddc91fad711562691
siddagra <52118936+siddagra@users.noreply.github.com>
2024-01-02 03:35:48 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,101 @@
+import torch
+import torch.nn as nn
+from torchvision.datasets import MNIST
+from torchvision import transforms 
+from torchvision.utils import save_image
+from torch.utils.data import DataLoader
+from torch.optim import AdamW
+from torch.optim.lr_scheduler import OneCycleLR
+from model import MNISTDiffusion
+from utils import ExponentialMovingAverage
+import os
+import math
+import argparse
+import random
+
+def create_mnist_dataloaders(batch_size,image_size=28,num_workers=4):
+    
+    preprocess=transforms.Compose([transforms.Resize(image_size),\
+                                    transforms.ToTensor(),\
+                                    transforms.Normalize([0.5],[0.5])]) #[0,1] to [-1,1]
+
+    train_dataset=MNIST(root="./mnist_data",\
+                        train=True,\
+                        download=True,\
+                        transform=preprocess
+                        )
+    test_dataset=MNIST(root="./mnist_data",\
+                        train=False,\
+                        download=True,\
+                        transform=preprocess
+                        )
+
+    return DataLoader(train_dataset,batch_size=batch_size,shuffle=True,num_workers=num_workers),\
+            DataLoader(test_dataset,batch_size=batch_size,shuffle=True,num_workers=num_workers)
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Training MNISTDiffusion")
+    parser.add_argument('--lr',type = float ,default=0.001)
+    parser.add_argument('--batch_size',type = int ,default=256)    
+    parser.add_argument('--epochs',type = int,default=100)
+    parser.add_argument('--ckpt',type = str,help = 'define checkpoint path',default='')
+    parser.add_argument('--n_samples',type = int,help = 'define sampling amounts after every epoch trained',default=256)
+    parser.add_argument('--model_base_dim',type = int,help = 'base dim of Unet',default=64)
+    parser.add_argument('--timesteps',type = int,help = 'sampling steps of DDPM',default=1000)
+    parser.add_argument('--model_ema_steps',type = int,help = 'ema model evaluation interval',default=10)
+    parser.add_argument('--model_ema_decay',type = float,help = 'ema model decay',default=0.995)
+    parser.add_argument('--log_freq',type = int,help = 'training log message printing frequence',default=10)
+    parser.add_argument('--no_clip',action='store_true',help = 'set to normal sampling method without clip x_0 which could yield unstable samples')
+    parser.add_argument('--cpu',action='store_true',help = 'cpu training')
+    args = parser.parse_args()
+
+    return args
+
+
+def main(args):
+    device="cpu" if args.cpu else "cuda"
+    train_dataloader,test_dataloader=create_mnist_dataloaders(batch_size=args.batch_size,image_size=28)
+    model=MNISTDiffusion(timesteps=args.timesteps,
+                image_size=28,
+                in_channels=1,
+                base_dim=args.model_base_dim,
+                dim_mults=[2,4]
+            ).to(device)
+    """print(model)
+    print("trainable params:", f'{sum(p.numel() for p in model.parameters() if p.requires_grad):,}' )
+    for name, param in model.named_parameters():
+        if param.requires_grad:
+            print(name, f'{sum(p.numel() for p in param if p.requires_grad):,}')"""
+    #torchvision ema setting
+    #https://github.com/pytorch/vision/blob/main/references/classification/train.py#L317
+    adjust = 1* args.batch_size * args.model_ema_steps / args.epochs
+    alpha = 1.0 - args.model_ema_decay
+    alpha = min(1.0, alpha * adjust)
+    model_ema = ExponentialMovingAverage(model, device=device, decay=1.0 - alpha)
+
+    optimizer=AdamW(model.parameters(),lr=args.lr)
+    scheduler=OneCycleLR(optimizer,args.lr,total_steps=args.epochs*len(train_dataloader),pct_start=0.25,anneal_strategy='cos')
+    loss_fn=nn.MSELoss(reduction='mean')
+
+    #load checkpoint
+    if args.ckpt:
+        ckpt=torch.load(args.ckpt, map_location='cuda:0')
+        model_ema.load_state_dict(ckpt["model_ema"])
+        model.load_state_dict(ckpt["model"])
+
+    global_steps=0
+
+
+    count = 0
+    model_ema.eval()
+    for j in range(0, (10000//args.n_samples)+1):
+        samples=model_ema.module.sampling(args.n_samples,clipped_reverse_diffusion=not args.no_clip,device=device)
+        for image in samples:
+            save_image(image,"sampeled_images/image_{:0>8}.png".format(count))
+            count += 1
+
+if __name__=="__main__":
+    args=parse_args()
+    main(args)
